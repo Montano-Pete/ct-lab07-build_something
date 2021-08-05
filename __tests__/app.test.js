ed1e3770d13a06d177f062c025f9ed59d7e399ae
Peter <pete225848@gmail.com>
2021-08-05 11:38:52 -0700

    passing static test for POST route (green)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,23 +2,23 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Pie from '../lib/models/Pie.js';
 
-describe('demo routes', () => {
+describe('pie routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('creates a pie via POST', () => {
+  it('creates a pie via POST', async () => {
     const pie = { type: 'Strawberry Rhubarb', wholePie: false, slice: true, sliceQuantity: 2 };
 
-    const res = request(app)
+    const res = await request(app)
       .post('/api/v1/pies')
       .send(pie);
 
     expect(res.body).toEqual({
       id: '1',
       ...pie
+    });
   });
-
-
 });
